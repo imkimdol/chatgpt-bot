7332e02fa91bd8a4a093ac180fc128803b2f78a6
lodmik <kimdol@proton.me>
2023-01-23 23:44:54 -0800

    Add followup support

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ await chatGPTAPI.initSession();
 
 
 // Discord init
+let result = null;
+
 const query = {
     data: new SlashCommandBuilder()
         .setName('query')
@@ -30,20 +32,48 @@ const query = {
     async execute(interaction) {
         const input = interaction.options.getString('input');
         await interaction.deferReply();
-        const result = await chatGPTAPI.sendMessage(input)
+        result = await chatGPTAPI.sendMessage(input)
 		await interaction.editReply(input);
         await interaction.followUp(result.response);
 }};
 
+const followUp = {
+    data: new SlashCommandBuilder()
+        .setName('followup')
+        .setDescription('Follow up to the previous ChatGPT query.')
+        .addStringOption(option =>
+            option
+                .setName('input')
+                .setDescription('The message to follow up with.')
+                .setMaxLength(2000)
+                .setRequired(true)),
+    async execute(interaction) {
+        if (result == null) {
+            await interaction.reply("No previous message to follow up on.");
+        } else {
+            const input = interaction.options.getString('input');
+            await interaction.deferReply();
+            result = await chatGPTAPI.sendMessage(input, {
+                conversationId: result.conversationId,
+                parentMessageId: result.messageId
+            });
+            await interaction.editReply(input);
+            await interaction.followUp(result.response);
+        }
+        
+}};
+
+
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 await rest.put(
     Routes.applicationCommands(process.env.CLIENT_ID),
-    { body: [query.data.toJSON()] },
+    { body: [query.data.toJSON(), followUp.data.toJSON()] },
 );
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 client.commands.set("query", query);
+client.commands.set("followup", followUp);
 
 client.login(process.env.DISCORD_TOKEN);
 
